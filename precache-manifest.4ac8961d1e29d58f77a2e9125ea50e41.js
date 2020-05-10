55cea44a0806e50d8c8c1b2106e643fb4de6ed00
cvspinelli <cvspinelli09@gmail.com>
2020-05-11 00:48:10 +0100

    Updates

---
+++ precache-manifest.4ac8961d1e29d58f77a2e9125ea50e41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc8a64250ec87e13dda80ddbc32c3ae",
+    "url": "/cvspinelli09/spinelli-my-website/index.html"
+  },
+  {
+    "revision": "c2fac8b23a2dd88fbd4c",
+    "url": "/cvspinelli09/spinelli-my-website/static/css/main.f3cdc0a5.chunk.css"
+  },
+  {
+    "revision": "364a6052a3216c98bbc6",
+    "url": "/cvspinelli09/spinelli-my-website/static/js/2.12b29d24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cvspinelli09/spinelli-my-website/static/js/2.12b29d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2fac8b23a2dd88fbd4c",
+    "url": "/cvspinelli09/spinelli-my-website/static/js/main.d5ea9d9c.chunk.js"
+  },
+  {
+    "revision": "cc704a95bebd2fb2842e",
+    "url": "/cvspinelli09/spinelli-my-website/static/js/runtime-main.7b5ba43c.js"
+  },
+  {
+    "revision": "f20cffd8f710c79289234ca63bf2d422",
+    "url": "/cvspinelli09/spinelli-my-website/static/media/LogoImg_small.f20cffd8.svg"
+  }
+]);
